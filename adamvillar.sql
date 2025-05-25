c4d1c4a854ecddb727b7eb3c9c809a4a10efab0b
Adam-Villar <adamvillar2013@gmail.com>
2025-05-25 14:26:12 +0200

    Create adamvillar.sql

---
+++ adamvillar.sql
@@ -0,0 +1,18 @@
+--table of user 
+--select :
+select * from user 
+--update : UPDATE table_name
+SET column1 = value1, column2 = value2
+
+--delete :
+ delete * from table
+
+
+
+
+select * from Review
+
+SET column1 = value1, column2 = value2
+
+delete * from table
+
