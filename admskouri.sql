19a0e22c409d8d4bba37ccae4d496d18ea062cfc
adamskouri <nxterra525@gmail.com>
2025-05-25 13:28:09 +0100

    Add files via upload

---
+++ admskouri.sql
@@ -0,0 +1,42 @@
+--table of restaurent owner
+--select:
+select * for restaurent owner
+--update:UPDATE table_name
+SET column1 = value1, column2 = value2
+--delete:
+* from table
+
+--table of restaurent
+--select:
+select * for restaurent
+--update:UPDATE table_name
+SET column1 = value1, column2 = value2
+--delete:
+* from table
+
+--table tourist
+--select:
+select * for tourist
+--update:UPDATE table_name
+SET column1 = value1, column2 = value2
+--delete:
+* from table 
+
+--table of menuitem
+--select:
+select * for menuitem
+--update:UPDATE table_name
+SET column1 = value1, column2 = value2
+--delete:
+* from table
+
+--table of photo 
+--select:
+select * for photo
+--update:UPDATE table_name
+SET column1 = value1, column2 = value2
+--delete:
+* from table
+
+
+
