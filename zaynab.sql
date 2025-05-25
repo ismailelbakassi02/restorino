8884b831963e5146a1ad03f8edb6595f28155244
zaynabelaiadi03 <elaiadizaynab@gmail.com>
2025-05-25 13:24:47 +0100

    Create zaynab.sql

---
+++ zaynab.sql
@@ -0,0 +1,16 @@
+--table ingredient
+--select:
+select* from ingredient
+--update:update table_name:
+SET column1 = value1, column2 = value2
+--delete:
+delete* from ingredient
+
+
+--table menuitemreview
+--select:
+select* from menuitemreview
+--update:update table_name:
+SET column1 = value1, column2 = value2
+--delete:
+delete* from menuitemreview
